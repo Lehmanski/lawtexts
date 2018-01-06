4f47569a56f1793b7fadcab855f451f8d02974fb
lehmanski <colehmann@uos.de>
2018-01-06 17:26:44 +0100

    changed name

---
+++ jsons/productClasses_MRL.py
@@ -0,0 +1,41 @@
+class Maschine():
+	def __init__(self):
+		self.text = '' 
+		self.HTML_Resource = None
+
+class UnvollstMaschine():
+	def __init__(self):
+		self.text = '' 
+		self.HTML_Resource = None
+
+class Sicherheitsbauteil():
+	def __init__(self):
+		self.text = '' 
+		self.HTML_Resource = None
+
+class Lastenaufnahmemittel():
+	def __init__(self):
+		self.text = '' 
+		self.HTML_Resource = None
+
+class KettenSeileGurte():
+	def __init__(self):
+		self.text = '' 
+		self.HTML_Resource = None
+
+class Werkzeug():
+	def __init__(self):
+		self.text = '' 
+		self.HTML_Resource = None
+
+class AuswAusruestung():
+	def __init__(self):
+		self.text = '' 
+		self.HTML_Resource = None
+
+class Gelenkwelle():
+	def __init__(self):
+		self.text = '' 
+		self.HTML_Resource = None
+
+

458bb892bc2a859c6e274523f7b0e7c6825680cd
lehmanski <colehmann@uos.de>
2018-01-06 16:08:35 +0100

    list of products excluded from MRL

---
+++ jsons/MRLExceptionList.py
@@ -0,0 +1,63 @@
+MRLExceptions = """
+a)
+Sicherheitsbauteile, die als Ersatzteile zur Ersetzung identischer Bauteile bestimmt sind und die vom Hersteller der Ursprungsmaschine geliefert werden;
+
+b)
+spezielle Einrichtungen für die Verwendung auf Jahrmärkten und in Vergnügungsparks;
+
+c)
+speziell für eine nukleare Verwendung konstruierte oder eingesetzte Maschinen, deren Ausfall zu einer Emission von Radioaktivität führen kann;
+
+d)
+Waffen einschließlich Feuerwaffen;
+
+e)
+die folgenden Beförderungsmittel:
+
+—	land- und forstwirtschaftliche Zugmaschinen in Bezug auf die Risiken, die von der Richtlinie 2003/37/EG erfasst werden, mit Ausnahme der auf diesen Fahrzeugen angebrachten Maschinen,
+
+—	Kraftfahrzeuge und Kraftfahrzeuganhänger im Sinne der Richtlinie 70/156/EWG des Rates vom 6. Februar 1970 zur Angleichung der Rechtsvorschriften der Mitgliedstaaten über die Betriebserlaubnis für Kraftfahrzeuge und Kraftfahrzeuganhänger (13)mit Ausnahme der auf diesen Fahrzeugen angebrachten Maschinen,
+
+—	Fahrzeuge im Sinne der Richtlinie 2002/24/EG des Europäischen Parlaments und des Rates vom 18. März 2002 über die Typgenehmigung für zweirädrige oder dreirädrige Kraftfahrzeuge (14)mit Ausnahme der auf diesen Fahrzeugen angebrachten Maschinen,
+
+—	ausschließlich für sportliche Wettbewerbe bestimmte Kraftfahrzeuge und
+
+—	Beförderungsmittel für die Beförderung in der Luft, auf dem Wasser und auf Schienennetzen mit Ausnahme der auf diesen Beförderungsmitteln angebrachten Maschinen;
+
+f)
+Seeschiffe und bewegliche Offshore-Anlagen sowie Maschinen, die auf solchen Schiffen und/oder in solchen Anlagen installiert sind;
+
+g)
+Maschinen, die speziell für militärische Zwecke oder zur Aufrechterhaltung der öffentlichen Ordnung konstruiert und gebaut wurden;
+
+h)
+Maschinen, die speziell für Forschungszwecke konstruiert und gebaut wurden und zur vorübergehenden Verwendung in Laboratorien bestimmt sind;
+
+i)
+Schachtförderanlagen;
+
+j)
+Maschinen zur Beförderung von Darstellern während künstlerischer Vorführungen;
+
+k)
+elektrische und elektronische Erzeugnisse folgender Arten, soweit sie unter die Richtlinie 73/23/EWG des Rates vom 19. Februar 1973 zur Angleichung der Rechtsvorschriften der Mitgliedstaaten betreffend elektrische Betriebsmittel zur Verwendung innerhalb bestimmter Spannungsgrenzen (15)fallen:
+
+—	für den häuslichen Gebrauch bestimmte Haushaltsgeräte,
+
+—	Audio- und Videogeräte,
+
+—	informationstechnische Geräte,
+
+—	gewöhnliche Büromaschinen,
+
+—	Niederspannungsschaltgeräte und -steuergeräte,
+
+—	Elektromotoren;
+
+l)
+die folgenden Arten von elektrischen Hochspannungsausrüstungen:
+
+—	Schalt- und Steuergeräte,
+
+—	Transformatoren.
+"""

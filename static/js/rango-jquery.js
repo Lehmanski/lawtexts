658270d501af59d841635e0f4e0d0611329ccd67
NilusvanEdel <ledelkott@uos.de>
2018-01-11 11:29:16 +0100

    all

--- static/js/rango-jquery.js
+++ static/js/rango-jquery.js
@@ -1,6 +1,15 @@
 $(document).ready( function() {
-
     $("#about-btn").click( function(event) {
-        alert("You clicked the button using JQuery!");
+    	
+    });
+
+    $('.btn-edit').click( function(event) {
+    	var id = this.id.match(/\d/g).join("");
+        alert("You clicked to edit item ".concat(id));
+	});
+
+    $('.btn-remove').click( function(event) {
+    	var id = this.id.match(/\d/g).join("");
+    	window.location.href= "/del/".concat(id)
     });
 });
